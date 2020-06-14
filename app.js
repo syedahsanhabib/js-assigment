9944210748b028cbd623990e808a719af15ec066
syedahsanhabib <syedahsanhabib021@gmail.com>
2020-06-14 18:01:34 +0500

    now ready to commit

---
+++ app.js
@@ -0,0 +1,48 @@
+//chapter 1
+//task 1
+// alert("Hello world");
+
+//task 2
+// alert("Error! Please enter a valid password.");
+
+
+//task 3
+// alert(" Welcome to JS land \r\n  Happy Coding...");
+
+
+
+
+//chapter 2
+//task 1
+// var UsreName ;
+// var Fullname = "ahsan habib";
+// var massage = "Hello world" ;
+// var age = "15 year old";
+// alert(massage);
+// alert(Fullname);
+// alert(age);
+
+//task 2
+// var email = "syedahsanhabib021@gmail.com";
+// alert("your email address " + email);
+
+//task 3
+// var book = "A smarter way to learn JavaScript";
+// alert(book);
+
+
+
+//task 4
+document.write("yah! i can write HTML content through in javascript");
+
+
+// var c = "Muhammad";
+// var d = "Habib";
+// var e = b+c+a+d
+// alert(e)
+
+//task 3
+// var ahsanHabib = 50;
+// alert( ahsanHabib )
+
+//task 
