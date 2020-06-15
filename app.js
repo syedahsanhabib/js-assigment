7695524ee53805568524fb097197b02415bec68c
syedahsanhabib <64634269+syedahsanhabib@users.noreply.github.com>
2020-06-15 12:50:14 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //chapter 1
+
+
 //task 1
 // alert("Hello world");
 
@@ -13,6 +15,8 @@
 
 
 //chapter 2
+
+
 //task 1
 // var UsreName ;
 // var Fullname = "ahsan habib";
@@ -30,19 +34,94 @@
 // var book = "A smarter way to learn JavaScript";
 // alert(book);
 
-
-
 //task 4
-document.write("yah! i can write HTML content through in javascript");
+// document.write("yah! i can write HTML content through in javascript");
 
 
-// var c = "Muhammad";
-// var d = "Habib";
-// var e = b+c+a+d
-// alert(e)
+//Chapter 3
 
-//task 3
-// var ahsanHabib = 50;
-// alert( ahsanHabib )
 
-//task 
+// Task 1
+
+	// var age = 15;
+	// alert("i am" + age + " years old.");
+
+// Task 2
+// 
+// 	var age = 15;
+// 	alert("i am" + age + " years old.");
+// 
+// Task 3
+// 
+//   var birthyear = 1990;
+//   document.write("My birthyear is 1990 <br> Data type of my ddeclared variable is number") 
+// 
+// Task 4
+// 
+	// var VisitorName = "John Doe";
+	// var ProductTitle = "T-Shirt";
+	// var Quantity = 5;
+	// document.write(VisitorName + " ordered " + Quantity + " "+ProductTitle + " on XYZ Clothing store");
+// 
+
+// chapter 4
+
+
+// Task 1
+// document.write("Variable name can only contain , number ,$ and_.For example : $My Firstvariable <br> Variable must begin with a latter ,$ or_.For example :$mane,_ name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords ");
+
+
+//  Chapter 5
+
+
+//  Task 1
+
+	// var a = 15;
+	// var b = 10;
+	// document.write("Sum of A=" + a + " and B= "+b + " is " + (a+b));
+
+//  Task 2
+// 
+	// var a = 15;
+	// var b = 10;
+	// alert("Subtraction of A=" + a + " and B= "+b + " is " + (a-b));
+
+//  Task 3
+// 
+// 	var a = 15;
+// 	var b = 10;
+// 	alert("Multiplication of A=" + a + " and B= "+b + " is " + (a*b));
+// 
+//  Task 4
+// 
+// 	var a = 15;
+// 	var b = 10;
+// 	alert("Division of A=" + a + " and B= "+b + " is " + (a/b));
+// 
+//  Task 5
+// 
+// 	var a ;
+// 	alert("Value after variable declaration is:" + a);
+// 
+//  Task 6
+
+//  b 1st
+//  var a ;
+// 	alert("Value after variable declaration is:" + a);
+// 
+//  Task 7
+// 
+// 	var a = 12;
+// 	alert("Value after variable declaration is: " + 12%3);
+// 
+//  Task 8
+// 
+// 	var F = 98;
+// 	var C = (F-32)*5/9;
+// 	alert("The value of Celcius is: " + C);
+// 
+// Task 9
+// 
+// 	var a = 12;
+// 	alert("Value after variable declaration is: " + 12%3);
+// 
